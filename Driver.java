6cd71d545173619d32a3d5af8c4252ef2a4c99c1
Kayla Fang <kfang00@stuy.edu>
2018-12-11 14:36:45 -0500

    altered Driver.java

--- Driver.java
+++ Driver.java
@@ -14,6 +14,9 @@ public class Driver{
     if(artie[1].equals("bubble")){
       Sorts.bubbleSort(randish);
     }
+    if(artie[1].equals("insertion")){
+      Sorts.insertionSort(randish);
+    }
     if(artie[1].equals("test")){
       int[] randish2 = Arrays.copyOf(randish,randish.length);
       int[] randish3 = Arrays.copyOf(randish,randish.length);
